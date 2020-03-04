838242531d220512dbaafcbea8640fce1bb4d861
Sam andalib <hesam.andalib@gmail.com>
2020-03-04 15:35:48 -0600

    add

---
+++ client/build/precache-manifest.b08f3b87d56141abaf90444ae134b696.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d13d42984530d893b4aa30d0f10f1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24d39d5afdb7c082f664",
+    "url": "/static/css/main.234f04dd.chunk.css"
+  },
+  {
+    "revision": "c83b895599cc5aead44b",
+    "url": "/static/js/2.1b05185e.chunk.js"
+  },
+  {
+    "revision": "a28b79ec84b62c853206d2f2b4b7c2d4",
+    "url": "/static/js/2.1b05185e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24d39d5afdb7c082f664",
+    "url": "/static/js/main.4a424366.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
